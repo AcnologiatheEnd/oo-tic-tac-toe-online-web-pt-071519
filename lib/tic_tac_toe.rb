ca1d60be22b0203ca721b7825fbd8ee2a9048881
Winston  Chung <18.chung.winston@ctkprep.org>
2019-08-22 07:16:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -99,13 +99,13 @@ class TicTacToe
   end
   
   def winner
-    WIN_COMBINATIONS.each do |routes|
+    WIN_COMBINATIONS.detect do |routes|
       if @board[routes[0]] == "X" &&  @board[routes[1]] == "X" &&  @board[routes[2]] == "X"
        return "X"
       elsif @board[routes[0]] == "O" &&  @board[routes[1]] == "O" &&  @board[routes[2]] == "O"
        return "O"
       else
-        return nil
+        nil
       end
     end
   end
