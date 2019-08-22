5727a8192951c68fd6b6be8b373720d93e9fda7f
Winston  Chung <18.chung.winston@ctkprep.org>
2019-08-22 07:14:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -105,7 +105,7 @@ class TicTacToe
       elsif @board[routes[0]] == "O" &&  @board[routes[1]] == "O" &&  @board[routes[2]] == "O"
        return "O"
       else
-        puts "no winner"
+        puts nil
       end
     end
   end
